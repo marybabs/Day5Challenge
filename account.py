76c8dd02273e54aad405a5e18ccbfee5519fb15e
marybabs <babjun@gmail.com>
2018-09-21 19:31:54 +0300

    Add Day five files

---
+++ account.py
@@ -0,0 +1,43 @@
+class BankAccount:
+
+    def __init__(self):
+        self.balance = 0
+        self.status = True
+
+    def open(self):
+        self.status = True
+    
+    def get_balance(self):
+        if self.status == False:
+            raise ValueError('account closed')
+
+        return self.balance
+
+    def deposit(self,amount):
+        if self.status == False:
+            raise ValueError('account closed')
+
+        if amount < 0 :
+            raise ValueError('wrong value')
+
+        self.balance += amount
+
+    def withdraw( self,amount):
+        if self.status == False:
+            raise ValueError('account closed')
+
+
+        if amount < 1:
+            raise ValueError('wrong value')
+
+        if amount > self.balance:
+            raise ValueError('less balance')
+            
+        self.balance-=amount
+        
+    def close(self):
+        self.status = False
+    
+    
+    
+        
